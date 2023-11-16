67d8adbc93b9c2a3b6838936f3e077d8f98846d0
Kelvin Mun <kmunsw@gmail.com>
2023-11-16 10:43:45 +0800

    Create main.tf

---
+++ k10/main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
